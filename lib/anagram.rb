f137a1cf6177a3888006a58064bf93a0acd7ff07
Tricky Licky <wangombevictor5@gmail.com>
2022-10-27 12:14:41 +0300

    complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  
+    def initialize(word)
+        @word = word
+    end
+
+   def match(sentence)
+      sentence.select do |word|
+          word.chars.sort == @word.chars.sort
+      end
+    end
+end
